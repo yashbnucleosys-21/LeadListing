11d3fd78cce6a94c5df069b45040dd54d0f22830
yashbnucleosys-21 <yashb.nucleosys@gmail.com>
2025-09-11 12:53:29 +0530

    Fix CORS allowed origins

--- LeadBackend--main/index.js
+++ LeadBackend--main/index.js
@@ -133,5 +133,6 @@ app.get('/', (req, res) => {
 
 // Start server
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
+  // console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server is running on https://lead-listing.vercel.app`);
 });
